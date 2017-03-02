fabd166bb77ffb9d86024b55376ceef9765603f9
Kevin <kevin.duglue@etu.unice.fr>
2017-03-02 16:51:34 +0100

    LGInverter without for loop and while loop

--- Mutator/src/main/java/mutation/LGInverterProcessor.java
+++ Mutator/src/main/java/mutation/LGInverterProcessor.java
@@ -1,27 +1,29 @@
 package mutation;
 
 import spoon.processing.AbstractProcessor;
-import spoon.reflect.code.BinaryOperatorKind;
-import spoon.reflect.code.CtBinaryOperator;
-import spoon.reflect.code.CtExpression;
-import spoon.reflect.code.CtFor;
+import spoon.reflect.code.*;
 import spoon.support.reflect.code.CtCommentImpl;
 
 
 public class LGInverterProcessor extends AbstractProcessor<CtBinaryOperator> {
 
     public void process(CtBinaryOperator binaryOperator) {
-        if(binaryOperator.getKind() == BinaryOperatorKind.LT && !(binaryOperator.getParent() instanceof CtFor)){
-            binaryOperator.setKind(BinaryOperatorKind.GT);
+
+
+        if(!(binaryOperator.getParent() instanceof CtFor) && !(binaryOperator.getParent() instanceof CtWhile)){
+            if(binaryOperator.getKind() == BinaryOperatorKind.LT){
+                binaryOperator.setKind(BinaryOperatorKind.GT);
+            }
+            else if(binaryOperator.getKind() == BinaryOperatorKind.LE){
+                binaryOperator.setKind(BinaryOperatorKind.GE);
+            }
+            else if(binaryOperator.getKind() == BinaryOperatorKind.GE){
+                binaryOperator.setKind(BinaryOperatorKind.LE);
+            }
+            else if(binaryOperator.getKind() == BinaryOperatorKind.GT){
+                binaryOperator.setKind(BinaryOperatorKind.LT);
+            }
         }
-        else if(binaryOperator.getKind() == BinaryOperatorKind.LE && !(binaryOperator.getParent() instanceof CtFor)){
-            binaryOperator.setKind(BinaryOperatorKind.GE);
-        }
-        else if(binaryOperator.getKind() == BinaryOperatorKind.GE && !(binaryOperator.getParent() instanceof CtFor)){
-            binaryOperator.setKind(BinaryOperatorKind.LE);
-        }
-        else if(binaryOperator.getKind() == BinaryOperatorKind.GT && !(binaryOperator.getParent() instanceof CtFor)){
-            binaryOperator.setKind(BinaryOperatorKind.LT);
-        }
+
     }
 }
