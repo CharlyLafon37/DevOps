560393b8947249f2ea2562e2ce31c3a53b69a06c
Kevin <kevin.duglue@etu.unice.fr>
2017-03-17 16:46:57 +0100

    Artifactory pom.xml

--- CookieFactory/j2e/CustomerRegistry/src/main/java/fr/unice/polytech/isa/tcf/CustomerFinder.java
+++ CookieFactory/j2e/CustomerRegistry/src/main/java/fr/unice/polytech/isa/tcf/CustomerFinder.java
@@ -1,9 +1,9 @@
 package fr.unice.polytech.isa.tcf;
 
-import fr.unice.polytech.isa.tcf.entities.Customer;
+		import fr.unice.polytech.isa.tcf.entities.Customer;
 
-import javax.ejb.Local;
-import java.util.Optional;
+		import javax.ejb.Local;
+		import java.util.Optional;
 
 @Local
 public interface CustomerFinder {
