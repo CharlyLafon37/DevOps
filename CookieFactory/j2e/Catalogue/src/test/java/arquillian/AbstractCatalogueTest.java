c9eda5df629a464bf7851d9d7b51923c38e3811a
Kevin Duglué <kevin.duglue@etu.unice.fr>
2017-04-01 19:35:40 +0200

    KCC Test

---
+++ CookieFactory/j2e/Catalogue/src/test/java/arquillian/AbstractCatalogueTest.java
@@ -0,0 +1,32 @@
+package arquillian;
+
+import fr.unice.polytech.isa.tcf.CartModifier;
+import fr.unice.polytech.isa.tcf.CatalogueExploration;
+import fr.unice.polytech.isa.tcf.asynchronous.KitchenPrinter;
+import fr.unice.polytech.isa.tcf.components.CartBean;
+import fr.unice.polytech.isa.tcf.components.CatalogueBean;
+import fr.unice.polytech.isa.tcf.components.carts.CartStatefulBean;
+import fr.unice.polytech.isa.tcf.entities.Customer;
+import fr.unice.polytech.isa.tcf.utils.Database;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+
+import javax.ejb.EJB;
+
+public abstract class AbstractCatalogueTest {
+
+
+	@Deployment
+	public static WebArchive createDeployment() {
+		// Building a Web ARchive (WAR) containing the following elements:
+		return ShrinkWrap.create(WebArchive.class)
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+				.addPackage(CatalogueBean.class.getPackage())
+				.addPackage(CatalogueExploration.class.getPackage())
+				;
+	}
+
+}
