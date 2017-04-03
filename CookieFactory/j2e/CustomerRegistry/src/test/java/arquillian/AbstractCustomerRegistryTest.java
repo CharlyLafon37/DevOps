747502229bbc16ffcde9dc83e86020659f5e8556
Anthonny Giroud <giroud.anthonny@gmail.com>
2017-04-03 11:08:38 +0200

    tests customer registry

---
+++ CookieFactory/j2e/CustomerRegistry/src/test/java/arquillian/AbstractCustomerRegistryTest.java
@@ -0,0 +1,44 @@
+package arquillian;
+
+import fr.unice.polytech.isa.tcf.CartModifier;
+import fr.unice.polytech.isa.tcf.CustomerFinder;
+import fr.unice.polytech.isa.tcf.asynchronous.KitchenPrinter;
+import fr.unice.polytech.isa.tcf.components.CartBean;
+import fr.unice.polytech.isa.tcf.components.CustomerRegistryBean;
+import fr.unice.polytech.isa.tcf.components.carts.CartStatefulBean;
+import fr.unice.polytech.isa.tcf.entities.Customer;
+import fr.unice.polytech.isa.tcf.exceptions.AlreadyExistingCustomerException;
+import fr.unice.polytech.isa.tcf.interceptors.LogParameters;
+import fr.unice.polytech.isa.tcf.managed.CustomerBean;
+import fr.unice.polytech.isa.tcf.utils.Database;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+
+import javax.ejb.EJB;
+
+public abstract class AbstractCustomerRegistryTest {
+
+
+	@EJB
+	protected Database memory;
+
+	@Deployment
+	public static WebArchive createDeployment() {
+		// Building a Web ARchive (WAR) containing the following elements:
+		return ShrinkWrap.create(WebArchive.class)
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+
+				.addPackage(CustomerRegistryBean.class.getPackage())
+				.addPackage(AlreadyExistingCustomerException.class.getPackage())
+				.addPackage(CustomerBean.class.getPackage())
+				.addPackage(CustomerFinder.class.getPackage())
+
+				// Persistence file
+				.addAsManifestResource(new ClassLoaderAsset("META-INF/persistence.xml"), "persistence.xml");
+	}
+
+}
